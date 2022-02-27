e4842cd8c01f07a1fc8c14301b6af26e8fed822d
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2022-02-28 01:06:09 +0900

    채팅 테스트

---
+++ src/main/java/com/example/demo/cnofig/RedisPublisher.java
@@ -0,0 +1,17 @@
+package com.example.demo.cnofig;
+
+import com.example.demo.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+// import 생략...
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
