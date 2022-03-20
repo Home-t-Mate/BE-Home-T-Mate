00d84fc5df8c26d30c1d952239b4534062542c46
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2022-03-20 21:23:52 +0900

    게시글 수정 로직 변경

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -14,6 +14,7 @@ import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.PotoRepository;
 import com.example.demo.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -134,129 +135,23 @@ public class PostService {
         return postRepository.save(post);
     }
 
-//    // 게시글 작성
-//    @Transactional
-//    public Post createPost(PostRequestDto postRequestDto, User user) {
-//
-//        if (postRequestDto.getPostImg() == null) {
-//            throw new IllegalArgumentException("이미지를 넣어주세요.");
-//        }
-//
-//        System.out.println("1차 break");
-//        String content = postRequestDto.getContent();
-//        if (postRequestDto.getContent() == null) {
-//            throw new IllegalArgumentException("내용을 입력해주세요.");
-//        }
-//        if (content.length() > 600) {
-//            throw new IllegalArgumentException("600자 이하로 입력해주세요.");
-//        }
-//
-//        System.out.println("2차");
-//
-//        String postImg = postRequestDto.getPostImg();
-//        Post post = new Post(content ,postImg, user);
-//
-//        System.out.println(post);
-//        return postRepository.save(post);
-//    }
 
+    public void updatePost(List<MultipartFile> multipartFile, String content, Long postId, User user) throws IOException {
 
-    // 게시글 수정
-//    @Transactional
-//    public PostResponseDto updatePost(List<MultipartFile> images,
-//                                          PostRequestDto requestDto,
-//                                          Long postId,
-//                                          User user
-//    ) {
-//        Post post = postRepository.findById(postId).orElseThrow(
-//                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다")
-//        );
-//
-//        if (!user.getId().equals(post.getUser().getId())) {
-//            throw new IllegalArgumentException("게시글을 작성한 유저만 수정이 가능합니다.");
-//        }
-//
-//        // 삭제할 사진 아이디 리스트
-//        List<Long> photoIdList = requestDto.getPhotoIdList();
-//
-//        // 게시물에 저장돼있던 사진 객체 리스트
-//        List<Photo> photoList = photoRepository.findAllByPost(post);
-//
-//        for (Photo photo : photoList) {
-//            if (photoIdList.contains(photo.getId())) {
-//                try {
-//                    String oldImageUrl = URLDecoder.decode(photo.getFilePath().replace("https://skifriendbucket.s3.ap-northeast-2.amazonaws.com/", ""), "UTF-8");
-//                    s3Uploader.deleteFromS3(oldImageUrl);
-//                    photoRepository.deleteById(photo.getId());
-//                } catch (Exception ignored) {
-//                }
-//            }
-//        }
-//        savePhotoList(images, post);
-//        post.update(requestDto);
-//        return generatePostResponseDto(post);
-//    }
+   //여기부터 해야함함
+        Post post = postRepository.getById(postId);
+        if (post == null) {
+            throw new IllegalArgumentException("해당 게시물이 존재하지 않습니다.");
+        }
+        potoRepository.deleteByPost(post);
 
-//    @Transactional
-//    public ResponseEntity<?> updatePost(Long id, PutRequestDto postRequestDto, UserDetailsImpl userDetails)
-//    {
-//        Post post = postRepository.findById(id).orElseThrow(
-//                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
-//        );
-//
-//        User user = post.getUser();
-//
-//        System.out.println(userDetails.getUser().getId());
-//        System.out.println(user.getId());
-//
-//        if (!userDetails.getUser().getId().equals(user.getId()))
-//        {
-//            throw new IllegalArgumentException("작성자만 수정할 수 있습니다.");
-//        }
-//
-//        if (postRequestDto.getContent() == null) {
-//            throw new IllegalArgumentException("내용을 입력해주세요.");
-//        }
-//        if (postRequestDto.getContent().length() > 600) {
-//            throw new IllegalArgumentException("600자 이하로 입력해주세요.");
-//        }
-//        post.UpdatePost(postRequestDto);
-//
-//        // 기존에 있던 이미지 파일 S3에서 삭제
-//        for ( PostFile deleteS3 : post.getFileList())
-//        {
-//            String[] fileKey = deleteS3.getUrl().split("static/");
-//            try
-//            {
-//                String decodeKey = URLDecoder.decode(fileKey[1], "UTF-8");
-//                s3Uploader.deleteS3("static/" + decodeKey);
-//            }
-//            catch (UnsupportedEncodingException e)
-//            {
-//                e.printStackTrace();
-//            }
-//        }
-//        // 기존에 있던 포스트파일 제거
-//        postFileRepository.deleteByPost(post);
-//
-//        // 새로운 이미지 파일 url 배열로 for 반복문을 실행
-//        for ( String url : postRequestDto.getFile())
-//        {
-//            // 이미지 파일 url로 postFile 객체 생성
-//            PostFile fileUrl = new PostFile(url);
-//
-//            // postFile에 savePost를 연관관계 설정
-//            fileUrl.setPost(post);
-//
-//            // 이미지 파일 url 1개에 해당되는 postFile을 DB에 저장
-//            PostFile saveFile = postFileRepository.save(fileUrl);
-//
-//            // 저장된 postFile을 저장된 post에 한개씩 추가함
-//            post.getFileList().add(saveFile);
-//        }
-//
-//        return ResponseEntity.ok().body(new BasicResponseDto("true"));
-//    }
+        post.update(content, user);
+        for(MultipartFile image : multipartFile) {
+            String postImg = s3Uploader.upload(image, "static");
+            Poto poto = new Poto(postImg, post);
+            potoRepository.save(poto);
+        }
+    }
 
 
     //게시글 삭제
@@ -276,7 +171,7 @@ public class PostService {
         }
         likeRepository.deleteByPost(post);
         postRepository.deleteById(postId);
+        potoRepository.deleteByPost(post);
         return postId;
     }
-
 }
