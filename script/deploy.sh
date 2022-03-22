ab56d5334530b51a0ef7b192bafb687e367bff55
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2022-03-23 00:51:09 +0900

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/opt/app
 cd $REPOSITORY
 
-APP_NAME=homeTmate
+APP_NAME=demo
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
