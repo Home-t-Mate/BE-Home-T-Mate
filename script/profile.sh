ad65b2d0f35d2dd5280b1086c77399f24e1674cc
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2022-03-23 23:28:29 +0900

    무중단 배포 테스트1.1

--- script/profile.sh
+++ script/profile.sh
@@ -4,7 +4,7 @@
 function find_idle_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [${RESPONSE_CODE} -ge 400] # 400 보다 크면(즉, 40x/50x 에러 모두 포함)
+  if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
 
   then
     CURRENT_PROFILE=real2
