15b54d378e3938a95f5d3af3d59c150fad720f9f
hgu5141 <93611963+hgu5141@users.noreply.github.com>
2022-03-28 11:12:01 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -123,7 +123,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 홈
         skipPathList.add("GET,/");
-//         skipPathList.add("GET,/chat/roomsscroll");
+         skipPathList.add("GET,/chat/roomsscroll");
 //         skipPathList.add("GET,/chat/roomsscroll/**");
         
 
